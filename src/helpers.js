cc877aed3137d089c06e7df03ff9afce06b40c3e
Ruslan Kyba <kybargr@gmail.com>
2019-01-07 17:38:28 +0200

    fix: proper game key generation

--- src/helpers.js
+++ src/helpers.js
@@ -8,12 +8,15 @@
 function generateGameKey(length = 4, keys = Object.keys(global.games)) {
   let key = '';
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  while (Object.keys(keys).indexOf(key) > -1) {
+
+  do {
     key = '';
     for (let i = 0; i < length; i++) {
       key += possible.charAt(Math.floor(Math.random() * possible.length));
     }
+    keys.push(key);
   }
+  while (Object.keys(keys).length === 0 || Object.keys(keys).indexOf(key) > -1);
 
   return key;
 }
