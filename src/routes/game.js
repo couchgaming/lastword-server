f125a11fe6a1ce53031dee63370991866ddbd16b
Ruslan Kyba <kybargr@gmail.com>
2019-01-07 17:38:53 +0200

    feat: add game routes

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
 const Game = require('../game');
 const { generateGameKey } = require('../helpers');
 
@@ -13,6 +15,21 @@ module.exports = (app) => {
     const key = generateGameKey();
     global.games[key] = new Game();
 
-    res.send({ key });
+    const token = jwt.sign({ name: `host-${uuid.v4()}`, key, host: true }, process.env.JWT_SECRET, {
+      expiresIn: 24 * 60 * 60 * 1000 // 1 day
+    });
+
+    res.send({ token, key });
+  });
+
+  /**
+   * Read game with the key
+   *
+   * @name New game
+   * @route {GET} /game/:key
+   */
+  app.get('/game/:key', (req, res) => {
+    // game info
+    res.send({});
   });
 };
